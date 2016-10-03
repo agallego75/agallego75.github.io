382e2159964e0984fff00ef25384c8178329a5bc
Alejandro Gallego <Alejandro Gallego>
2016-10-02 22:10:48 -1000

    update

--- contact.php
+++ contact.php
@@ -1,32 +1,29 @@
 <?php
-$field_name = $_POST['cf_name'];
-$field_email = $_POST['cf_email'];
-$field_message = $_POST['cf_message'];
-
-$mail_to = 'a.white.gallego@gmail.com';
-$subject = 'Message from a site visitor '.$field_name;
-
-$body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: '.$field_email."\n";
-$body_message .= 'Message: '.$field_message;
-
-$headers = 'From: '.$field_email."\r\n";
-$headers .= 'Reply-To: '.$field_email."\r\n";
-
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
-
-if ($mail_status) { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Thank you for the message. We will contact you shortly.');
-		window.location = 'contact_page.html';
-	</script>
-<?php
-}
-else { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Message failed. Please, send an email to gordon@template-help.com');
-		window.location = 'contact_page.html';
-	</script>
-<?php
-}
+  $name = $_POST['cf_name'];
+  $visitor_email = $_POST['cf_email'];
+  $message = $_POST['body'];
 ?>
+
+<?php
+    $email_from = 'agallego75@gmail.com';
+ 
+    $email_subject = "New Form submission";
+ 
+    $email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message".
+?>
+
+<?php
+ 
+  $to = "agallego75@gmail.com";
+ 
+  $headers = "From: $email_from \r\n";
+ 
+  $headers .= "Reply-To: $visitor_email \r\n";
+ 
+  mail($to,$email_subject,$email_body,$headers);
+ 
+?>
+
+
+
